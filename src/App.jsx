f3297c633f74c5a3cd67e4e2ec8960b759380165
Ali Ashraf <aaaaaa30101975@gmail.com>
2025-04-14 16:28:08 +0200

    fix structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@
 // import viteLogo from "/vite.svg";
 // import "./App.css";
 // import { AArrowDown } from "lucide-react";
-import CourseDetails from './pages/CourseDetails'
-import CourseList from './pages/CourseList'
+import CourseList from './pages/CourseList/CourseList.jsx'
 import Quiz from "./component/quiz/quiz.jsx";
 import Leaderboard from "./component/Leaderboard/Leaderboard.jsx";
 import Login from "./pages/LoginScreen/Login.jsx";
 import Signup from "./pages/SignUpScreen/signUp.jsx";
+import CourseDetails from './pages/CourseDetails/CourseDetails.jsx';
 function App() {
 
   return (
