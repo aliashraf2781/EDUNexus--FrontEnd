cb115ed3d139dce076c17c36af6980ac74e93937
Yasmeen-Saad <yasmeensaad626@gmail.com>
2025-04-17 02:38:11 +0200

    Add the CourseLesson Page, Complete the CourseDetails Page and Edit CourseList Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import CourseDetails from './pages/CourseDetails/CourseDetails'
+import CourseLesson from './pages/CourseLesson/CourseLesson'
 import CourseList from './pages/CourseList/CourseList'
 
 function App() {
   return (
     // <CourseList />
-    <CourseDetails />
+    // <CourseDetails />
+    <CourseLesson />
   )
 }
 
