dc75524ea10dca523b74e8883125dfccc5163572
Yasmeen-Saad <yasmeensaad626@gmail.com>
2025-04-18 23:47:17 +0200

    Add InstructorProfile

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import CourseDetails from './pages/CourseDetails/CourseDetails'
 import CourseLesson from './pages/CourseLesson/CourseLesson'
 import CourseList from './pages/CourseList/CourseList'
+import InstructorProfile from './pages/InstructorProfile/InstructorProfile'
 
 function App() {
   return (
     // <CourseList />
     // <CourseDetails />
-    <CourseLesson />
+    // <CourseLesson />
+    <InstructorProfile />
   )
 }
 
